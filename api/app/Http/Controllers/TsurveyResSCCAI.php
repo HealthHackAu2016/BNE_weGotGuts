f8b02d47c0942b169e129a4ffc6873a41261e915
Komal Bandi <info@pidataconsulting.com.au>
2016-10-15 17:33:15 +1000

    Still owrking.

---
+++ api/app/Http/Controllers/TsurveyResSCCAI.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Laravel\Lumen\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+use LOG;
+
+class TsurveyRESSCCAI extends BaseController
+{
+    public function insert(Request $request)
+    {
+        $idResponder = $request->idResponder;
+        $dtSubmit = $request->dtSubmit;
+        $genWellbeing = $request->genWellbeing;
+        $abdPain = $request->abdPain;
+        $lqdStoolFreq = $request->lqdStoolFreq;
+        $adbMass = $request->adbMass;
+        $jointProb = $request->jointProb;
+        $eyeProb = $request->eyeProb;
+        $mouthProb = $request->mouthProb;
+        $skinProbUlcers = $request->skinProbUlcers;
+        $skinProbRedBumps = $request->skinProbRedBumps;
+        $perianalProb = $request->perianalProb;
+        $fistula = $request->fistula;
+
+        try {
+            app('db')->insert('INSERT INTO `tsurveyreshbi`
+                            (`idResponder`,
+                            `dtSubmit`,
+                            `genWellbeing`,
+                            `abdPain`,
+                            `lqdStoolFreq`,
+                            `adbMass`,
+                            `jointProb`,
+                            `eyeProb`,
+                            `mouthProb`,
+                            `skinProbUlcers`,
+                            `skinProbRedBumps`,
+                            `perianalProb`,
+                            `fistula`)
+                            VALUES
+                            (?,?,?,?,?,?,?,?,?,?,?,?,?)',
+                [$idResponder, $dtSubmit, $genWellbeing, $abdPain, $lqdStoolFreq, $adbMass, $jointProb, $eyeProb, $mouthProb, $skinProbUlcers, $skinProbRedBumps, $perianalProb, $fistula]);
+
+        } catch (\Exception $e) {
+            $result = array(
+                'Result' => 'ERROR',
+                'Message' => 'Unable to connect to the database, please try again later.'
+            );
+
+            app('log')->debug($e->getMessage());
+            echo json_encode($result);
+            return;
+        }
+
+        $result = array(
+            'Result' => 'OK',
+            'Message' => 'Survey Inserted'
+        );
+        echo json_encode($result);
+        return;
+    }
+}
