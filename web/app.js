4d3b255c063d60c315e514f443983ada6cc0b33a
Mark Promnitz <mark@Ferdinand.local>
2016-10-16 11:30:50 +1000

    Additions to website

--- web/app.js
+++ web/app.js
@@ -33,6 +33,7 @@ const homeController = require('./controllers/home');
 const userController = require('./controllers/user');
 const apiController = require('./controllers/api');
 const contactController = require('./controllers/contact');
+const visController = require('./controllers/visualisations');
 
 /**
  * API keys and Passport configuration.
@@ -131,6 +132,11 @@ app.post('/account/profile', passportConfig.isAuthenticated, userController.post
 app.post('/account/password', passportConfig.isAuthenticated, userController.postUpdatePassword);
 app.post('/account/delete', passportConfig.isAuthenticated, userController.postDeleteAccount);
 app.get('/account/unlink/:provider', passportConfig.isAuthenticated, userController.getOauthUnlink);
+
+
+
+app.get('/visualisations', visController.getVisualisations);
+
 
 /**
  * API examples routes.
---
+++ web/controllers/visualisations.js
@@ -0,0 +1,69 @@
+const dygraphs = require('dygraphs');
+const mysql = require('mysql');
+const async = require('async');
+
+
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'guts',
+  password : 'gutsHH2016',
+  database : 'hh_guts_public'
+});
+
+connection.connect();
+
+// connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+//   if (err) throw err;
+//   console.log('The solution is: ', rows[0].solution);
+// });
+
+connection.end();
+
+
+exports.getVisualisations = (req, res) => {
+
+  var _sql_statement = `select tibdocreading.idResponder as idResponder,
+tibdocreading.dtSubmit as dtSubmit,
+tibdocreading.Reading as calpro,
+tsurveyreshbi.genWellbeing +
+tsurveyreshbi.abdPain +
+tsurveyreshbi.lqdStoolFreq +
+tsurveyreshbi.adbMass +
+tsurveyreshbi.jointProb +
+tsurveyreshbi.eyeProb +
+tsurveyreshbi.mouthProb +
+tsurveyreshbi.skinProbUlcers +
+tsurveyreshbi.skinProbRedBumps +
+tsurveyreshbi.perianalProb +
+tsurveyreshbi.fistula as hbi_score
+from hh_guts_public.tibdocreading, hh_guts_public.tsurveyreshbi
+where tibdocreading.idResponder = tsurveyreshbi.idResponder
+and tibdocreading.dtSubmit  = tsurveyreshbi.dtSubmit
+and tibdocreading.idResponder = "C0089"`
+
+  connection.query(_sql_statement, function(err, rows, fields) {
+    if (err) throw err;
+
+    console.log('Result: ', rows);
+    var _data = [];
+    async.each(rows, function(file, callback) {
+      console.log(rows);
+      _data.push(row);
+        //process row
+    }, function(err) {
+        // if any of the file processing produced an error, err would equal that error
+        if( err ) {
+          // One of the iterations produced an error.
+          // All processing will now stop.
+          console.log('Error processing results:', err);
+        } else {
+          console.log('All results have been processed successfully');
+          res.render('vis/visualisations', {
+            title: 'Patient Visualisations',
+            data: _data
+          });
+        }
+    });
+  });
+};
